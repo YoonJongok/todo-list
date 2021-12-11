ccb1ac00974f8593784344d755caabcf46ec54a2
Jongok Yoon <bak01076@gmail.com>
2021-12-11 23:04:44 +0100

    add list done

--- src/components/todoList.tsx
+++ src/components/todoList.tsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { ButtonHTMLAttributes } from "react";
 import { useForm } from "react-hook-form";
+import { atom, useRecoilState } from "recoil";
 interface IForm {
   toDo: string;
 }
 
+export enum CategoryEnum {
+  "TO_DO",
+  "Doing",
+  "Done",
+}
+interface IToDo {
+  id: number;
+  text: string;
+  category: CategoryEnum;
+}
+
+const todoState = atom<IToDo[]>({
+  key: "toDos",
+  default: [],
+});
+
 export const ToDoList = () => {
+  const [toDos, setTodos] = useRecoilState(todoState);
   const { register, handleSubmit, setValue } = useForm<IForm>();
-  const handleValid = (data: IForm) => {
-    console.log("add to do", data.toDo);
+  const handleValid = ({ toDo }: IForm) => {
+    console.log("add to do", toDo);
     setValue("toDo", "");
+    setTodos((oldTodo) => [
+      { text: toDo, id: Date.now(), category: CategoryEnum.TO_DO },
+      ...oldTodo,
+    ]);
+  };
+
+  const handleBtnClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event.currentTarget.value);
   };
   return (
     <div>
@@ -20,6 +46,22 @@ export const ToDoList = () => {
           placeholder="Write a to do"
         />
         <button>Add</button>
+        <ul>
+          {toDos.map((todo) => (
+            <li key={todo.id}>
+              {todo.text}
+              <button value={CategoryEnum.TO_DO} onClick={handleBtnClick}>
+                {CategoryEnum[0]}
+              </button>
+              <button value={CategoryEnum.Doing} onClick={handleBtnClick}>
+                {CategoryEnum[1]}
+              </button>
+              <button value={CategoryEnum.Done} onClick={handleBtnClick}>
+                {CategoryEnum[2]}
+              </button>
+            </li>
+          ))}
+        </ul>
       </form>
     </div>
   );
