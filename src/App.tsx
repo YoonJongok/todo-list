dbc2bf2f164f53336d0e1008f24799860f84897c
Jongok Yoon <bak01076@gmail.com>
2021-12-11 15:56:03 +0100

    Project setup for the todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import React from "react";
 import { createGlobalStyle } from "styled-components";
-import ToDoList from "./todoList";
+import { ToDoList } from "./components/todoList";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
